c6a208ffc3952d83606ccf6eda200c6a794dac98
AnnaAnnakie <anna.marchetti77@gmail.com>
2024-06-12 15:01:09 +0200

    besoin de tester une fois hébergé

--- img.js
+++ img.js
@@ -4,33 +4,35 @@ let big = document.getElementById("false");
 
 // pour chaque item set un clicklistener
 item.forEach(function (img) {
-  if (big.id == "true") {
-  } else {
-    img.addEventListener("click", clic(img));
-  }
+  img.addEventListener("click", function () {
+    let displayImg = document.createElement("img");
+    displayImg.src = img.src;
+    displayImg.alt = "tnaga";
+    displayImg.style.width = "80vh";
+    displayImg.style.position = "absolute";
+    displayImg.style.zIndex = "999";
+    displayImg.className = "deletable";
+    document.body.appendChild(displayImg);
+
+    // assombrir
+    let overlay = document.createElement("div");
+    overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+    overlay.style.width = "100%";
+    overlay.style.height = "100%";
+    overlay.style.zIndex = "99";
+    overlay.style.position = "absolute";
+    overlay.style.top = "0";
+    overlay.className = "overlay";
+    // pour éclaicir
+    overlay.onclick = function quit() {
+      eclaire();
+      document.body.removeChild(document.querySelector(".deletable"));
+    };
+    document.body.appendChild(overlay);
+    // fixation de la page
+    body.style.overflow = "hidden";
+    body.style.overflowY = "scroll !important";
+    // désactiver le clic listener
+    img.removeEventListener("click", this);
+  });
 });
-
-function clic(img) {
-  img.style.width = "fit-content";
-  img.style.position = "absolute";
-  img.style.zIndex = "999";
-
-  // assombrir
-  let overlay = document.createElement("div");
-  overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  overlay.style.width = "100%";
-  overlay.style.height = "100%";
-  overlay.style.zIndex = "99";
-  overlay.style.position = "absolute";
-  overlay.style.top = "0";
-  overlay.className = "overlay";
-  document.body.appendChild(overlay);
-
-  // fixation de la page
-  body.style.overflow = "hidden";
-  body.style.overflowY = "scroll !important";
-
-  // désactiver le clic listener
-  img.removeEventListener(click);
-  big.id = "true";
-}
